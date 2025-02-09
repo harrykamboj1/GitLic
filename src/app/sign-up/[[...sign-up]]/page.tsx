46f7fb877ba8f3a12107e79961719745a280b1d9
harrykamboj1 <singhharnoor116@gmail.com>
2025-02-09 17:57:10 +0530

    SHADCN COMPONENTS ADDED || CLERK AUTH || MIDDLEWARE SETUP

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,35 @@
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function SignUpPage() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold">Welcome</h1>
+        </div>
+        <p className="text-base text-zinc-400">
+          Log in or Create account to get back to your dashboard
+        </p>
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp
+              appearance={{
+                elements: {
+                  footer: "hidden",
+                },
+              }}
+            />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-gradient-to-bl from-blue-500 via-blue-500 to-purple-500 lg:flex">
+        <Image src={"/logo.png"} alt="Logo" height={300} width={300} />
+      </div>
+    </div>
+  );
+}
